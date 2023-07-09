1ad5ca262e014c829566dfa388507b606fa7164d
agronkercishta <agronkercishta.ai@gmail.com>
2023-07-08 21:05:14 -0400

    modify

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import streamlit as st
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain, SequentialChain
+from langchain.memory import ConversationBufferMemory
+from langchain.utilities import WikipediaAPIWrapper
 from apikey import apikey
 
 os.environ['OPENAI_API_KEY'] = apikey
@@ -19,18 +21,35 @@ title_template = PromptTemplate(
 )
 
 script_template = PromptTemplate(
-    input_variables = ['title'],
-    template = 'write me a youtube video script based on this title TITLE: {title}'
+    input_variables = ['title', 'wikipedia_research'],
+    template = 'write me a youtube video script based on this title TITLE: {title} while leveraging this wikipedia research:{wikipedia_research}'
 )
+
+# Memory
+title_memory = ConversationBufferMemory(input_key = 'topic', memory_key = 'chat_history')
+script_memory = ConversationBufferMemory(input_key = 'title', memory_key = 'chat_history')
 
 # LLMs
 llm = OpenAI(temperature = 0.9)
-title_chain = LLMChain(llm = llm, prompt = title_template, verbose = True, output_key = 'title')
-script_chain = LLMChain(llm = llm, prompt = script_template, verbose = True, output_key = 'script')
-sequential_chain = SequentialChain(chains = [title_chain, script_chain], input_variables = ['topic'], output_variables = ['title', 'script'], verbose = True)
+title_chain = LLMChain(llm = llm, prompt = title_template, verbose = True, output_key = 'title', memory = title_memory)
+script_chain = LLMChain(llm = llm, prompt = script_template, verbose = True, output_key = 'script', memory = script_memory)
+
+wiki = WikipediaAPIWrapper()
 
 # Show stuff to the screen if there's a prompt
 if prompt:
-    response = sequential_chain({'topic' : prompt})
-    st.write(response['title'])
-    st.write(response['script'])
+    title = title_chain.run(prompt)
+    wiki_research = wiki.run(prompt)
+    script = script_chain.run(title = title, wikipedia_research = wiki_research)
+
+    st.write(title)
+    st.write(script)
+
+    with st.expander('Title History'):
+        st.info(title_memory.buffer)
+    
+    with st.expander('Script History'):
+        st.info(script_memory.buffer)
+
+    with st.expander('Wikipedia Research'):
+        st.info(wiki_research)
